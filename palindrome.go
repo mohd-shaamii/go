4ee480367acee51f4dc20417ac972c211fcc42c8
BitRebels <122071878+mohd-shaamii@users.noreply.github.com>
2023-09-10 13:36:14 +0530

    Update palindrome.go

--- palindrome.go
+++ palindrome.go
@@ -1,36 +1,42 @@
 package main
-import (
-	"fmt"
-)
-func isPalindrome(n int) bool {
-	original := n
-	reversed := 0
-	for n > 0 {
-		digit := n % 10
-		reversed = reversed*10 + digit
-		n /= 10
+import "fmt"
+func isPalindrome(num int) bool {
+	rev := 0
+	temp := num
+	for temp != 0 {
+		rev = rev*10 + temp%10
+		temp /= 10
 	}
-	return original == reversed
+	return rev == num
+}
+func largestPalindrome(lower, upper int) (int, int, int) {
+	large := 0
+	num1 := 0
+	num2 := 0
+	product := 0
+	for i := upper; i >= lower; i-- {
+		for j := upper; j >= lower; j-- {
+			product = i * j
+			if product >= large && isPalindrome(product) {
+				large = product
+				num1 = i
+				num2 = j
+			}
+		}
+	}
+	return large, num1, num2
 }
 func main() {
 	var lower, upper int
-	fmt.Print("Enter the lower bound of the range : ")
+	fmt.Print("Enter the lower bound of the range (e.g., 100): ")
 	fmt.Scan(&lower)
-	fmt.Print("Enter the upper bound of the range : ")
+	fmt.Print("Enter the upper bound of the range (e.g., 999): ")
 	fmt.Scan(&upper)
 	if lower < 100 || upper > 999 || lower > upper {
 		fmt.Println("Invalid range. Please enter valid 3-digit bounds.")
 		return
 	}
-	largestPalindrome := 0
-	for i := lower; i <= upper; i++ {
-		for j := lower; j <= upper; j++ {
-			product := i * j
-			if isPalindrome(product) && product > largestPalindrome {
-				largestPalindrome = product
-			}
-		}
-	}
-	fmt.Println("The largest palindrome product of two", lower, "-digit numbers is:", largestPalindrome)
+	large, num1, num2 := largestPalindrome(lower, upper)
+	fmt.Println("The largest Palindrome product: ", large)
+	fmt.Println("The Numbers: ", num1, num2)
 }
-
